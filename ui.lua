60772efc58de60ec801c76bf6f194fb5cdb811e1
rthyjk <96871930+rthyjk@users.noreply.github.com>
2024-06-07 22:29:27 +0700

    Update ui.lua

--- ui.lua
+++ ui.lua
@@ -1,5 +1,5 @@
 local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
-local Window = Library.CreateLib("hjklcch's fake admin script V1.0.0", " Ocean")
+local Window = Library.CreateLib("hjklcch's fake admin script V1.0.0", "Ocean")
 local Tab = Window:NewTab("Credit")
 local Section = Tab:NewSection("Credit")
 Section:NewLabel("If you having a problems contract me in discord")
